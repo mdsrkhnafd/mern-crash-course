8d015527a5bb6315409212dd10e235ba85546c0a
user.email <afridi4mudasir@gmail.com>
2025-03-07 16:47:24 +0500

    Fixed outDir placement for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://mern-crash-course-production-6544.up.railway.app",
       },
     },
   },
